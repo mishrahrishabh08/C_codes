41c36cf12c69f86b3c67ca470080896c47e6b7ab
mishrahrishabh08 <91203032+mishrahrishabh08@users.noreply.github.com>
2023-12-23 07:02:04 +0530

    Add files via upload
    Simple Notes

---
+++ simplenote.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <string.h>
+#include <sys/stat.h>
+
+void usage(char * program_name , char * filename){
+    printf("Usage: %s <data to add to %s>\n" , program_name, filename);
+    exit(0);
+}
+
+void fatal(char *);
+void * ec_malloc(unsigned int);
+
+// main function
+int main (int argc, char * argv[]){
+    int fd;
+    char * buffer , * datafile;
+    buffer = (char *)ec_malloc(100);
+    datafile = (char *)ec_malloc(20);
+    strcpy(datafile ,"/tmp/notes");
+
+    if(argv< 2){
+        usage(argv[0],datafile);
+    }
+
+    strcpy(buffer,argv[1]);
+    printf("[DEBUG] buffer @ %p : \'%s\'\n",buffer, buffer);
+    printf("[DEBUG] datafile @ %p : \'%s\'\n",datafile,datafile);
+    strncat(buffer,'\n',1);
+
+    //Opening a file
+    fd = open(datafile, O_WRONLY|O_CREAT|O_APPEND, S_IRUSR|S_IWUSR);
+    if(fd == -1)
+        fatal("in main() while opening file");
+    printf("[DEBUG] file descriptor is %d\n", fd);
+    // Writing data
+    if(fwrite(fd, buffer, strlen(buffer)) == -1)
+        fatal("in main() while writing buffer to file");
+    // Closing file
+    if(fclose(fd) == -1)
+        fatal("in main() while closing file");
+    printf("Note has been saved.\n");
+    free(buffer);
+    free(datafile);
+}
+// A function to display an error message and then exit
+void fatal(char *message) {
+    char error_message[100];
+    strcpy(error_message, "[!!] Fatal Error ");
+    strncat(error_message, message, 83);
+    perror(error_message);
+    exit(-1);
+}
+// An error-checked malloc() wrapper function
+void *ec_malloc(unsigned int size) {
+    void *ptr;
+    ptr = malloc(size);
+    if(ptr == NULL)
+    fatal("in ec_malloc() on memory allocation");
+    return ptr;
+}
